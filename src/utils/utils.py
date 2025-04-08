bf945a21279552ef5ddcfa5428ee2aa38c255f97
Mrinal Chakravarthy <mrinal.chakravarthy@orkes.io>
2025-04-08 14:34:02 -0700

    Adding function returns to functions for clarity

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,16 +1,16 @@
 import os
 import tiktoken
-from langchain.chat_models import init_chat_model
+from langchain.chat_models import init_chat_model, BaseChatModel
 from langchain.prompts import PromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
-def load_chat_model():
+def load_chat_model() -> BaseChatModel:
     OPENAI_API_BASE = os.environ.get("OPENAI_API_BASE")
     TEMPERATURE = os.environ.get("TEMPERATURE", "0.1")
     OPENAI_MODEL = os.environ.get("OPENAI_MODEL", "gpt-4o-mini")
     return init_chat_model(OPENAI_MODEL, model_provider="openai", base_url=OPENAI_API_BASE, temperature=float(TEMPERATURE))
 
-def messages_token_count(messages, model="gpt-4-turbo"):
+def messages_token_count(messages, model="gpt-4-turbo") -> int:
     encoding = tiktoken.encoding_for_model(model)
     num_tokens = 0
 
@@ -19,7 +19,7 @@ def messages_token_count(messages, model="gpt-4-turbo"):
         num_tokens += len(encoding.encode(content))
     return num_tokens
 
-def token_count(text, model_name="gpt-4o"):
+def token_count(text, model_name="gpt-4o") -> int:
     # Initialize the encoder for the specified model
     encoder = tiktoken.encoding_for_model(model_name)
     # Encode the text into tokens
@@ -44,7 +44,7 @@ def read_file_prompt(file_path: str) -> str:
         print(f"Error reading file {file_path}: {e}")
         return ""
 
-def reasoning_prompt(prompt_path: str, **input_vars):
+def reasoning_prompt(prompt_path: str, **input_vars) -> str:
     prompt_templ = read_file_prompt(prompt_path)
     prompt = PromptTemplate(
         template=prompt_templ, input_variables=list(input_vars.keys()),
@@ -52,10 +52,10 @@ def reasoning_prompt(prompt_path: str, **input_vars):
     message = prompt.format_prompt(**input_vars)
     return message.to_string()
 
-def get_last_k_human_messages(messages, k=1):
+def get_last_k_human_messages(messages, k=1) -> list[HumanMessage]:
     return [message for message in reversed(messages) if isinstance(message, HumanMessage)][:k]
 
-def get_latest_human_message(messages):
+def get_latest_human_message(messages) -> str:
     return get_last_k_human_messages(messages, 1)[0].content
 
 def parse_report_command(input_string: str) -> str:
