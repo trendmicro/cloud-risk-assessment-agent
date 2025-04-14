a3192ad695f5f174c9f00429858956b8fd1a793c
Mrinal Chakravarthy <mrinal.chakravarthy@orkes.io>
2025-04-14 13:46:38 -0700

    Removing LangGraph dependency

--- src/core/app.py
+++ src/core/app.py
@@ -1,19 +1,12 @@
-from io import StringIO
 from typing import Dict, Optional
-import pandas as pd # type: ignore
 
 # LangChain imports
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage # type: ignore
-from langgraph.graph import StateGraph, END, START # type: ignore
-from langgraph.checkpoint.memory import MemorySaver # type: ignore
-from langchain.schema.runnable.config import RunnableConfig # type: ignore
+from langchain_core.messages import HumanMessage, AIMessage # type: ignore
 
 # Chainlit imports
 import chainlit as cl # type: ignore
 
 # Local imports
-from src.core.agent_state import AgentState
-from src.core.node_functions import classify_user_intent, execute_db_query, generate_summary_report, generate_insights, finalize_conclusion, provide_explanation
 from src.db.db_setup import setup_database_connections
 from src.conductor.conductor import ConductorManager
 
@@ -21,35 +14,6 @@ from src.conductor.conductor import ConductorManager
 from fastapi import HTTPException, Response, APIRouter # type: ignore
 from chainlit.server import app # type: ignore
 from starlette.routing import BaseRoute, Route # type: ignore
-
-checkpointer=MemorySaver()
-
-#-------------------------------
-# Graph node
-#-------------------------------
-
-builder = StateGraph(AgentState)
-
-builder.add_node("intent", classify_user_intent)
-builder.add_node("querydb", execute_db_query)
-builder.add_node("summary", generate_summary_report)
-builder.add_node("insight", generate_insights)
-builder.add_node("conclude", finalize_conclusion)
-builder.add_node("reason", provide_explanation)
-
-# define the node which will display the reasoning result on web
-REASONING_NODE = ["reason", "summary", "insight", "conclude"]
-
-builder.add_edge(START, "intent")
-builder.add_edge("summary", "insight")
-builder.add_edge("insight", "conclude")
-builder.add_edge("querydb", "reason")
-builder.add_edge("conclude", END)
-builder.add_edge("reason", END)
-
-graph = builder.compile(
-checkpointer=checkpointer
-)
 
 #-------------------------------
 # Chainlit Authentication
@@ -71,36 +35,11 @@ async def on_chat_start():
 async def on_message(msg: cl.Message):
     chat_history = cl.user_session.get("chat_history")
     chat_history.append({"role": "user", "content": msg.content})
-    config = {"configurable": {"thread_id": msg.thread_id}}
 
     final_answer = cl.Message(content="")
-    
-    async for msg, metadata in graph.astream({"messages": [HumanMessage(content=msg.content)]}, stream_mode="messages", config=RunnableConfig(callbacks=[], **config)):
-        if (
-            msg.content
-            and not isinstance(msg, HumanMessage)
-            and not isinstance(msg, SystemMessage)
-            and metadata["langgraph_node"] in REASONING_NODE
-        ):
-            await final_answer.stream_token(msg.content)
 
-        if (
-            "finish_reason" in msg.response_metadata
-            and msg.response_metadata["finish_reason"] == "stop"
-        ):
-            await final_answer.stream_token("\n\n")
 
-        # Hack print report by dataframe
-        if (
-            "finish_reason" in msg.response_metadata
-            and msg.response_metadata["finish_reason"] == "stop"
-            and metadata["langgraph_node"] in ["insight"]
-        ):
-            state = graph.get_state(config=config)
-            df_str = state.values["dataframe"]
-            df = pd.read_csv(StringIO(df_str))
-            elements = [cl.Dataframe(data=df, display="inline", name="Dataframe")]
-            await cl.Message(content="Report Table:", elements=elements).send()
+            
 
     await final_answer.send()
 
@@ -147,14 +86,7 @@ async def on_chat_resume(thread):
                 state_messages.append(HumanMessage(content=message["content"]))
             else:
                 state_messages.append(AIMessage(content=message["content"]))
-
-        thread_id = thread["id"]
-        config = {"configurable": {"thread_id": thread_id}}
-        state = graph.get_state(config).values
-        #print(f'on_chat_resume: state = ', state)
-        if "messages" not in state:
-            state["messages"] = state_messages
-            graph.update_state(config, state)
+            
 
 
 cust_router = APIRouter()
